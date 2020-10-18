24b5a5320aebafd8110dbb703b3bc33712b524be
Lailatis Syarifah <49274639+Lilywas@users.noreply.github.com>
2020-10-18 18:57:04 +0700

    Update unit.php

--- application/views/admin/unit.php
+++ application/views/admin/unit.php
@@ -1,3 +1,23 @@
+<?php
+function esc_attr($text)
+{
+  $safe_text = wp_check_invalid_utf8($text);
+  $safe_text = _wp_specialchars($safe_text, ENT_QUOTES);
+  /**
+   * Filters a string cleaned and escaped for output in an HTML attribute.
+   *
+   * Text passed to esc_attr() is stripped of invalid or special characters
+   * before output.
+   *
+   * @since 2.0.6
+   *
+   * @param string $safe_text The text after it has been escaped.
+   * @param string $text      The text prior to being escaped.
+   */
+  return apply_filters('attribute_escape', $safe_text, $text);
+}
+?>
+
 <!-- Begin Page Content -->
 <div class="container-fluid">
   <h2 class="h3 mb-2 text-gray-800 text-center">Daftar Unit Kerja</strong></h2>
@@ -27,8 +47,8 @@
               foreach ($unit as $item) {
                 $no++; ?>
                 <tr>
-                  <td style="text-align: center;"><?= $no; ?></td>
-                  <td><?= $item['nama_unit']; ?></td>
+                  <td style="text-align: center;"><?= esc_attr($no); ?></td>
+                  <td><?= esc_attr($item['nama_unit']); ?></td>
                   <td style="vertical-align: middle; text-align: center;">
                     <!-- Tombol Edit -->
                     <button data-toggle="modal" data-target="#edit_unit<?= $item['id_unit']; ?>" class="btn btn-warning"><i class="far fa-fw fa-edit" title="Edit Unit kerja"></i></button>
